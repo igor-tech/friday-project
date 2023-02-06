36e1d5d025e9a98a1eb16e9445fa1b104473817e
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-06 23:14:55 +0300

    feat(theme): add global theme for fonts Montserrat

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
 
+import { ThemeProvider } from '@mui/material'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Provider } from 'react-redux'
 import { HashRouter } from 'react-router-dom'
 
 import App from './App/App'
+import { store } from './App/store'
+import { theme } from './common/constants/font-theme'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <Provider store={store}>
+    <HashRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </HashRouter>
+  </Provider>
 )
