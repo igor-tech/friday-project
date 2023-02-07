8168961a6a388284156ea6e4e835def8231c879a
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-07 17:42:42 +0300

    feat(store): add reducers and instance

--- src/App/store.ts
+++ src/App/store.ts
@@ -1,7 +1,15 @@
 import { AnyAction, configureStore } from '@reduxjs/toolkit'
 import { ThunkDispatch } from 'redux-thunk'
+
+import { setNewPasswordReducer } from '../features/Auth/RecoveryPasswordForms/NewPassword/newPassword-slice'
+import { recoveryPasswordReducer } from '../features/Auth/RecoveryPasswordForms/RecoveryPassword/recovery-password-slice'
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    recoveryPassword: recoveryPasswordReducer,
+    setNewPassword: setNewPasswordReducer,
+  },
+  devTools: true,
 })
 
 export type RootState = ReturnType<typeof store.getState>
