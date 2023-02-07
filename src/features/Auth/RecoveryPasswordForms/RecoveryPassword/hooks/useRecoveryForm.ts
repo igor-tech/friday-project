6bf19110ba205f51ef1a5b36a32ceb9766383c14
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-07 17:38:50 +0300

    feat(recovery): add layout and functional

---
+++ src/features/Auth/RecoveryPasswordForms/RecoveryPassword/hooks/useRecoveryForm.ts
@@ -0,0 +1,35 @@
+import { useFormik } from 'formik'
+import { useNavigate } from 'react-router-dom'
+import * as yup from 'yup'
+
+import { useAppDispatch, useAppSelector } from '../../../../../common'
+import { resetPassword } from '../recovery-password-slice'
+
+export const useRecoveryForm = () => {
+  const dispatch = useAppDispatch()
+  const isRecoveryPass = useAppSelector(state => state.recoveryPassword.isRecovery)
+  const navigate = useNavigate()
+  const { touched, resetForm, handleSubmit, errors, getFieldProps } = useFormik({
+    initialValues: {
+      email: '',
+    },
+    validationSchema: yup.object({
+      email: yup.string().email('Invalid email address'),
+    }),
+    onSubmit: (data: { email: string }): void => {
+      dispatch(resetPassword(data.email))
+      resetForm()
+    },
+  })
+
+  return {
+    dispatch,
+    isRecoveryPass,
+    navigate,
+    resetForm,
+    handleSubmit,
+    errors,
+    getFieldProps,
+    touched,
+  }
+}
