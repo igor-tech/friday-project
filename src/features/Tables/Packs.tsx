90844c0a67401bb64505dd5e6a0ae6b253fa64ae
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-14 21:00:23 +0300

    feat(Packs):crud create packs

--- src/features/Tables/Packs.tsx
+++ src/features/Tables/Packs.tsx
@@ -1,22 +1,43 @@
 import React, { useEffect } from 'react'
 
-import { useAppDispatch } from '../../common'
+import { Box, Typography } from '@mui/material'
 
+import { GeneralButton, useAppDispatch } from '../../common'
+
+import { addNewPackBtnSx, addPackContainerSx, packsContainerSx, packTitleSx } from './Packs.muiSx'
 import { TablePacks } from './Table-packs/TablePacks'
-import { getPacks } from './table-slice'
+import { createNewPack, getPacks } from './table-slice'
 
 const Packs = () => {
   const dispatch = useAppDispatch()
 
+  const addNewPack = () => {
+    const dataParams = {
+      name: `New Pack Name`,
+      deckCover: '',
+      private: false,
+    }
+
+    dispatch(createNewPack(dataParams))
+  }
+
   useEffect(() => {
-    dispatch(getPacks({}))
+    dispatch(getPacks())
   }, [])
 
   return (
-    <div>
+    <Box sx={packsContainerSx}>
+      <Box sx={addPackContainerSx}>
+        <Typography component="h1" sx={packTitleSx}>
+          Pack List
+        </Typography>
+        <GeneralButton name="Add new pack" sx={addNewPackBtnSx} onClick={addNewPack} />
+      </Box>
+
       <TablePacks />
+
       {/*<TableCards />*/}
-    </div>
+    </Box>
   )
 }
 
