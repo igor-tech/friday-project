1a4ed4da2b3f1f3219452852eb99b394585e1711
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-15 23:42:52 +0300

    refactor(): patch and name

--- src/features/Tables/FilterPanel/Filter-components/MyFilter/MyFilter.tsx
+++ src/features/Tables/FilterPanel/Filter-components/MyFilter/MyFilter.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 import { ToggleButton, ToggleButtonGroup, Typography } from '@mui/material'
 import Box from '@mui/material/Box/Box'
