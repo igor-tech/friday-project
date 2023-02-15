9e983a77bf3201588778cb66f61912bde4587b28
Igor Shargin <igor20513@gmail.com>
2023-02-15 21:43:54 +0500

    feat(TableCards): add table

--- src/features/Tables/Table-cards/TableCards.tsx
+++ src/features/Tables/Table-cards/TableCards.tsx
@@ -1,12 +1,21 @@
 import React, { useEffect } from 'react'
 
 import { Box, Typography } from '@mui/material'
+import Paper from '@mui/material/Paper'
+import Table from '@mui/material/Table'
+import TableContainer from '@mui/material/TableContainer'
 import { useSearchParams } from 'react-router-dom'
 
-import { GeneralButton, useAppDispatch, useAppSelector } from '../../../common'
+import arrow from '../../../assets/img/arrow.png'
+import { useAppDispatch, useAppSelector } from '../../../common'
+import { packsContainerSx } from '../Table-packs/Packs.muiSx'
+import { paperPacksSx } from '../Table-packs/TablePacks.muiSx'
 
-import arrow from './../../../assets/img/arrow.png'
-import { createNewCard, getCards, setPacksCardId } from './cards-slice'
+import { getCards, setPacksCardId } from './cards-slice'
+import { largeText, smallText, startPositionSx } from './Cards.muiSx'
+import { TableCardsBody } from './Table-cards-body/TableCardsBody'
+import { HeadersCards } from './Table-cards-head/TableCardsHead'
+import { EmptyPacks } from './Table-empty-packs/EmptyPacks'
 
 export const TableCards = () => {
   const dispatch = useAppDispatch()
@@ -16,7 +25,6 @@ export const TableCards = () => {
   const packName = useAppSelector(state => state.cards.packName)
   const isMy = myProfileId === myPackUserId
 
-  console.log(card)
   const { sortCards, cardQuestion, page, pageCount } = useAppSelector(
     state => state.cards.cardsQueryParams
   )
@@ -32,81 +40,28 @@ export const TableCards = () => {
   }, [sortCards, cardQuestion, page, pageCount])
 
   return (
-    <Box sx={{ margin: '24px 136px' }}>
-      <Box
-        sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', gap: '12px' }}
-      >
+    <Box sx={packsContainerSx}>
+      <Box sx={startPositionSx}>
         <Typography component="img" src={arrow} alt="arrow back" />
-        <Typography
-          component="h1"
-          variant="h5"
-          sx={{ fontWeight: '400', fontSize: '14px', lineHeight: '24px', color: '#000000' }}
-        >
+        <Typography component="h1" variant="h5" sx={smallText}>
           Back to Packs List
         </Typography>
       </Box>
-      {card?.length === 0 && <EmptyPacks packName={packName} isMyPack={isMy} />}
-      <div>Cards</div>
-    </Box>
-  )
-}
-
-const EmptyPacks: React.FC<{ packName: string; isMyPack: boolean }> = ({ packName, isMyPack }) => {
-  const dispatch = useAppDispatch()
-  const addNewCard = () => {
-    dispatch(createNewCard({}))
-  }
-
-  return (
-    <Box sx={{ marginTop: '27px' }}>
-      <Typography
-        sx={{ fontWeight: '600', fontSize: '22px', lineHeight: '27px', color: '#000000' }}
-        component="h2"
-      >
-        {packName}
+      <Typography sx={largeText} component="h2">
+        {card?.length !== 0 && (isMy ? 'My Pack' : 'Friend’s Pack')}
       </Typography>
-      <Box
-        sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '86px' }}
-      >
-        {isMyPack && (
-          <>
-            <Typography
-              component="p"
-              sx={{
-                fontSize: '16px',
-                lineHeight: '20px',
-                fontWeight: '400',
-                opacity: '0.5',
-                color: '#000000',
-              }}
-            >
-              This pack is empty. Click add new card to fill this pack
-            </Typography>
-            <GeneralButton
-              sx={{ padding: '8px 28px', marginTop: '32px' }}
-              name="Add new card"
-              onClick={addNewCard}
-            />
-          </>
-        )}
-
-        {!isMyPack && (
-          <>
-            <Typography
-              component="p"
-              sx={{
-                fontSize: '16px',
-                lineHeight: '20px',
-                fontWeight: '400',
-                opacity: '0.5',
-                color: '#000000',
-              }}
-            >
-              This pack is empty
-            </Typography>
-          </>
-        )}
-      </Box>
+      {card?.length === 0 ? (
+        <EmptyPacks packName={packName} isMyPack={isMy} />
+      ) : (
+        <Paper sx={paperPacksSx}>
+          <TableContainer>
+            <Table aria-labelledby="tableTitle">
+              <HeadersCards />
+              <TableCardsBody />
+            </Table>
+          </TableContainer>
+        </Paper>
+      )}
     </Box>
   )
 }
