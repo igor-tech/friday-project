feb48ee4107f7f02fce893f8b42c13e273ee1733
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-15 21:55:04 +0300

    feat(empty-search-message): in the wrong search

---
+++ src/features/Tables/Table-packs/EmptySearchMessage/searchPackNameParam.muiSx.ts
@@ -0,0 +1,12 @@
+export const containerEmptySearchPacksSx = {
+  marginTop: '50px',
+  display: 'flex',
+  justifyContent: 'center',
+}
+export const emptyPackNameSx = {
+  fontWeight: '400',
+  fontSize: '22px',
+  lineHeight: '35px',
+  color: '#000000',
+  opacity: '0.5',
+}
