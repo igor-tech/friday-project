04c80241ff128abd2d93eda94158d3b0a439c273
Igor Shargin <igor20513@gmail.com>
2023-02-15 21:36:14 +0500

    feat(Table cards): add cards style

---
+++ src/features/Tables/Table-cards/Cards.muiSx.ts
@@ -0,0 +1,52 @@
+export const startPositionSx = {
+  display: 'flex',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  gap: '12px',
+}
+
+export const grade = {
+  display: 'flex',
+  alignItems: 'center',
+}
+export const btnIcon = {
+  marginLeft: '40px',
+}
+
+export const smallText = {
+  fontWeight: '400',
+  fontSize: '14px',
+  lineHeight: '24px',
+  color: '#000000',
+}
+
+export const largeText = {
+  fontWeight: '600',
+  fontSize: '22px',
+  lineHeight: '27px',
+  color: '#000000',
+}
+
+export const lightText = {
+  fontSize: '16px',
+  lineHeight: '20px',
+  fontWeight: '400',
+  opacity: '0.5',
+  color: '#000000',
+}
+
+export const indentBtn = {
+  padding: '8px 28px',
+  marginTop: '32px',
+}
+
+export const locationText = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  marginTop: '86px',
+}
+
+export const marginBox = {
+  marginTop: '27px',
+}
