86c052eea185dd042fb9a50cb3b6d848cd57e2e2
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-17 14:32:02 +0300

    feat(card-menu): moved it to separate folders
                      changed imports

---
+++ src/features/Cards/Table-cards/CardMenu/CardHeaderMenu/cardHeaderMenu.muiSx.ts
@@ -0,0 +1,17 @@
+export const containerCardMenuSx = {
+  marginTop: '27px',
+  display: 'flex',
+  justifyContent: 'space-between',
+}
+export const containerPackMenuSx = {
+  display: 'flex',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+}
+export const packNameSx = {
+  fontWeight: '600',
+  fontSize: '22px',
+  lineHeight: '27px',
+  color: '#000000',
+}
+export const btnCardMenuSx = { padding: '8px 36px' }
