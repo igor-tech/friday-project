23085b8934c1b521a7bf562549cf186cf7db963a
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-17 03:10:40 +0300

    refactor(empty-search-message): moved to common components

--- src/common/components/EmptySearchMessage/EmptySearchMessage.tsx
+++ src/common/components/EmptySearchMessage/EmptySearchMessage.tsx
@@ -2,17 +2,13 @@ import React from 'react'
 
 import { Box, Typography } from '@mui/material'
 
-import { useAppSelector } from '../../../../common'
-
 import { containerEmptySearchPacksSx, emptyPackNameSx } from './searchPackNameParam.muiSx'
 
-export const EmptySearchMessage = () => {
-  const searchPackNameParam = useAppSelector(state => state.packs.packsQueryParams.packName)
-
+export const EmptySearchMessage: React.FC<{ searchParam: string }> = ({ searchParam }) => {
   return (
     <Box sx={containerEmptySearchPacksSx}>
       <Typography sx={emptyPackNameSx}>
-        Nothing was found with the given values {searchPackNameParam || ''} <br />
+        Nothing was found with the given values {searchParam || ''} <br />
         Change settings
       </Typography>
     </Box>
