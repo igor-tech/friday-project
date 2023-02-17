ff181be1a863dae622b012d826152c497fc7debd
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-17 14:32:37 +0300

    feat(empty-pack-menu): moved it to separate folders
                      changed imports

---
+++ src/features/Cards/Table-cards/EmptyPackMenu/emptyPackMenu.muiSx.ts
@@ -0,0 +1,22 @@
+export const containerPackMenuSx = { margin: '24px 136px' }
+export const containerMyPackMenuSx = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  marginTop: '86px',
+}
+export const packNameSx = {
+  fontWeight: '600',
+  fontSize: '22px',
+  lineHeight: '27px',
+  color: '#000000',
+  marginTop: '27px',
+}
+export const warningTitleSx = {
+  fontSize: '16px',
+  lineHeight: '20px',
+  fontWeight: '400',
+  opacity: '0.5',
+  color: '#000000',
+}
+export const btnSx = { padding: '8px 28px', marginTop: '32px' }
