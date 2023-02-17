3016ea64cce582b8eabd726287737f49575072fa
Igor Shargin <igor20513@gmail.com>
2023-02-17 20:04:40 +0500

    feat(Store): add redux persist

--- src/App/store.ts
+++ src/App/store.ts
@@ -1,4 +1,15 @@
-import { AnyAction, configureStore } from '@reduxjs/toolkit'
+import { AnyAction, combineReducers, configureStore } from '@reduxjs/toolkit'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 import { ThunkDispatch } from 'redux-thunk'
 
 import { authReducer } from '../features/Auth/auth-slice'
@@ -10,18 +21,34 @@ import { profileReducer } from '../features/Profile/profile-slice'
 
 import { appReducer } from './app-slice'
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  recoveryPassword: recoveryPasswordReducer,
+  setNewPassword: setNewPasswordReducer,
+  profile: profileReducer,
+  app: appReducer,
+  packs: packsReducer,
+  cards: CardsReducer,
+})
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    recoveryPassword: recoveryPasswordReducer,
-    setNewPassword: setNewPasswordReducer,
-    profile: profileReducer,
-    app: appReducer,
-    packs: packsReducer,
-    cards: CardsReducer,
-  },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   devTools: process.env.NODE_ENV === 'development',
 })
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = ThunkDispatch<RootState, any, AnyAction>
--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Provider } from 'react-redux'
 import { HashRouter } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import App from './App/App'
-import { store } from './App/store'
+import { persistor, store } from './App/store'
 import { theme } from './common/constants/font-theme'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
@@ -16,7 +17,9 @@ root.render(
   <Provider store={store}>
     <HashRouter>
       <ThemeProvider theme={theme}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </ThemeProvider>
     </HashRouter>
   </Provider>
