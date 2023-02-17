e30f10d4b335f0d998b226f75e62e0316567d3ca
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-17 14:53:17 +0300

    refactor(table-head): moved markup to a higher level

--- src/features/Packs/Table-packs/Table-packs-head/EnhancedTableHead.tsx
+++ src/features/Packs/Table-packs/Table-packs-head/EnhancedTableHead.tsx
@@ -9,25 +9,27 @@ import { headCellSx, tableHeadSx } from '../TablePacks.muiSx'
 export const HeadersPack = () => {
   const { orderBy, order, requestSortHandler, setSortHandler, headCells } = useTablePacksHead()
 
+  const headCellsLayout = headCells.map(headCell => (
+    <TableCell
+      key={headCell.id}
+      sortDirection={orderBy === headCell.id ? order : false}
+      onClick={() => requestSortHandler(headCell.id)}
+    >
+      <TableSortLabel
+        active={orderBy === headCell.id}
+        direction={orderBy === headCell.id ? order : 'asc'}
+        onClick={() => setSortHandler(headCell.id)}
+        IconComponent={ArrowDropDownIcon}
+      >
+        <Typography sx={headCellSx}>{headCell.label}</Typography>
+      </TableSortLabel>
+    </TableCell>
+  ))
+
   return (
     <TableHead sx={tableHeadSx}>
       <TableRow>
-        {headCells.map(headCell => (
-          <TableCell
-            key={headCell.id}
-            sortDirection={orderBy === headCell.id ? order : false}
-            onClick={() => requestSortHandler(headCell.id)}
-          >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : 'asc'}
-              onClick={() => setSortHandler(headCell.id)}
-              IconComponent={ArrowDropDownIcon}
-            >
-              <Typography sx={headCellSx}>{headCell.label}</Typography>
-            </TableSortLabel>
-          </TableCell>
-        ))}
+        {headCellsLayout}
         <TableCell sx={headCellSx}>Actions</TableCell>
       </TableRow>
     </TableHead>
