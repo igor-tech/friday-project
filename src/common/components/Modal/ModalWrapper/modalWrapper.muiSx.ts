33e56cf7043e5c0c4ac661de37f1c79b719e6c68
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-22 19:32:32 +0300

    feat(modalWrapper): modal window

---
+++ src/common/components/Modal/ModalWrapper/modalWrapper.muiSx.ts
@@ -0,0 +1,23 @@
+export const containerChildSx = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '395px',
+  bgcolor: 'background.paper',
+  border: '2px',
+  boxShadow: 24,
+}
+export const titleModalContainerSx = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+}
+export const titleModalSx = {
+  margin: '19px 0',
+  fontWeight: '500',
+  fontSize: '18px',
+  lineHeight: '22px',
+}
+export const closeExitIconSx = { cursor: 'pointer' }
+export const dividerSx = { position: 'absolute', top: '60px', width: '100%', right: '-15px' }
