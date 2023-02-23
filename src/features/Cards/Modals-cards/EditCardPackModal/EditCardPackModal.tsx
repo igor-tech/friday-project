b1eceeb9c4b7ba3d97f6cc42ba19ca2c2d8c2374
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-23 15:45:45 +0300

    feat(edit-card-pack-modal): modal window for editing the card pack

---
+++ src/features/Cards/Modals-cards/EditCardPackModal/EditCardPackModal.tsx
@@ -0,0 +1,104 @@
+import React, { ChangeEvent, useState } from 'react'
+
+import { Box, Checkbox, FormControlLabel, TextField } from '@mui/material'
+
+import { updateCardPack } from '../../cards-slice'
+
+import {
+  cancelBtnSx,
+  editPackCardBtnContainer,
+  editPackCardContainerSx,
+  editPackCardFormControlSx,
+  saveBtnSx,
+} from './editCardPackModal.muiSx'
+
+import {
+  appStatusSelector,
+  GeneralButton,
+  packIdSelector,
+  packNameCardSelector,
+  privateStatusSelector,
+  useAppDispatch,
+  useAppSelector,
+} from 'common'
+
+export const EditCardPackModal: React.FC<{ closeModal: () => void }> = ({ closeModal }) => {
+  const dispatch = useAppDispatch()
+  const packId = useAppSelector(packIdSelector)
+  const cardPackName = useAppSelector(packNameCardSelector)
+  const currentPrivateStatus = useAppSelector(privateStatusSelector)
+  const statusLoad = useAppSelector(appStatusSelector)
+  const [newPrivateStatus, setNewPrivateStatus] = useState(currentPrivateStatus)
+  const [newNamePackCard, setNewNamePackCard] = useState(cardPackName)
+  const [error, setError] = useState('')
+
+  const editPackCardHandler = () => {
+    if (newNamePackCard.trim() === cardPackName.trim()) {
+      setError("You didn't change your name")
+
+      return
+    }
+    if (newNamePackCard.trim() !== '' && !error) {
+      const updateCurrentCardPack = {
+        _id: packId,
+        name: newNamePackCard,
+        private: newPrivateStatus,
+      }
+
+      dispatch(updateCardPack(updateCurrentCardPack))
+        .unwrap()
+        .then(() => {
+          closeModal()
+        })
+    } else {
+      setError('Name must not be empty')
+    }
+  }
+  const setNamePackCardHandler = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const name = e.currentTarget.value
+
+    setNewNamePackCard(name)
+    setError('')
+  }
+
+  return (
+    <Box sx={editPackCardContainerSx}>
+      <TextField
+        fullWidth
+        type="text"
+        variant="standard"
+        label="Name pack"
+        value={newNamePackCard}
+        error={!!error}
+        helperText={error}
+        onChange={setNamePackCardHandler}
+        disabled={statusLoad === 'loading'}
+      />
+      <FormControlLabel
+        sx={editPackCardFormControlSx}
+        disabled={statusLoad === 'loading'}
+        control={
+          <Checkbox
+            onChange={e => setNewPrivateStatus(e.currentTarget.checked)}
+            checked={newPrivateStatus}
+          />
+        }
+        label="Private pack"
+      />
+      <Box sx={editPackCardBtnContainer}>
+        <GeneralButton
+          name="Cancel"
+          sx={cancelBtnSx}
+          onClick={closeModal}
+          disabled={statusLoad === 'loading'}
+        />
+        <GeneralButton
+          name="Save"
+          sx={saveBtnSx}
+          onClick={editPackCardHandler}
+          disabled={statusLoad === 'loading' || !!error}
+        />
+      </Box>
+    </Box>
+  )
+}
---
+++ src/features/Cards/Modals-cards/EditCardPackModal/editCardPackModal.muiSx.ts
@@ -0,0 +1,10 @@
+export const editPackCardContainerSx = { marginTop: '35px' }
+export const editPackCardFormControlSx = { marginTop: '30px' }
+export const editPackCardBtnContainer = {
+  margin: '34px 0 47px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+}
+export const cancelBtnSx = { padding: '8px 28px', background: '#FCFCFC', color: '#000000' }
+export const saveBtnSx = { padding: '8px 44px' }
