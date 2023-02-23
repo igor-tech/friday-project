abfd6b0c676349678e2e9767390434b69b10edd8
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-23 14:10:15 +0300

    refactor(modal-content): add entity for modal window

--- src/common/components/Modal/ModalContent/ModalContent.tsx
+++ src/common/components/Modal/ModalContent/ModalContent.tsx
@@ -4,7 +4,15 @@ import { MODAL_TYPE } from '../../../constants'
 import { useAppSelector } from '../../../hooks'
 import { modalTypeSelector } from '../../../selectors'
 
-import { AddNewPackModal, DeletePackModal, EditPackModal } from 'features'
+import {
+  AddNewPackModal,
+  DeletePackModal,
+  EditPackModal,
+  AddNewCardModal,
+  EditCardModal,
+  DeleteCardModal,
+  DeleteCardPackModal,
+} from 'features'
 
 export const ModalContent: React.FC<{ closeModal: () => void }> = ({ closeModal }) => {
   const typeModal = useAppSelector(modalTypeSelector)
@@ -14,6 +22,12 @@ export const ModalContent: React.FC<{ closeModal: () => void }> = ({ closeModal 
       {typeModal === MODAL_TYPE.addNewPack && <AddNewPackModal closeModal={closeModal} />}
       {typeModal === MODAL_TYPE.editCurrentPack && <EditPackModal closeModal={closeModal} />}
       {typeModal === MODAL_TYPE.deleteCurrentPack && <DeletePackModal closeModal={closeModal} />}
+      {typeModal === MODAL_TYPE.addNewCard && <AddNewCardModal closeModal={closeModal} />}
+      {typeModal === MODAL_TYPE.editCurrentCard && <EditCardModal closeModal={closeModal} />}
+      {typeModal === MODAL_TYPE.deleteCurrentCard && <DeleteCardModal closeModal={closeModal} />}
+      {typeModal === MODAL_TYPE.deleteCurrentPackCard && (
+        <DeleteCardPackModal closeModal={closeModal} />
+      )}
     </>
   )
 }
