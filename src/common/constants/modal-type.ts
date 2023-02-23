a829a261f5d0a791d7d8170d4c93080bddc2bc3f
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-23 14:05:04 +0300

    feat(empty-pack-menu): add type for card and card pack

--- src/common/constants/modal-type.ts
+++ src/common/constants/modal-type.ts
@@ -2,4 +2,11 @@ export enum MODAL_TYPE {
   addNewPack = 'addNewPack',
   editCurrentPack = 'editCurrentPack',
   deleteCurrentPack = 'deleteCurrentPack',
+
+  addNewCard = 'addNewCard',
+  editCurrentCard = 'editCurrentCard',
+  deleteCurrentCard = 'deleteCurrentCard',
+
+  deleteCurrentPackCard = 'deleteCurrentPackCard',
+  editCurrentPackCard = 'editCurrentPackCard',
 }
