25392ec38e7eed2df6a3d0d9e778549244ebae5d
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-23 13:15:41 +0300

    feat(delete-card) : modal for delete card

---
+++ src/features/Cards/Modals-cards/DeleteCardModal/DeleteCardModal.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+import { Box, Typography } from '@mui/material'
+
+import { deleteCard, setSettingDeleteCardModal } from '../../cards-slice'
+
+import {
+  cancelBtnSx,
+  deleteBtnSx,
+  deleteCardBtnContainerSx,
+  deleteCardContainerSx,
+  warningMessageSx,
+} from './deleteCardModal.muiSx'
+
+import {
+  appStatusSelector,
+  CardIdSettingSelector,
+  GeneralButton,
+  questionSettingSelector,
+  useAppDispatch,
+  useAppSelector,
+} from 'common'
+
+export const DeleteCardModal: React.FC<{ closeModal: () => void }> = ({ closeModal }) => {
+  const dispatch = useAppDispatch()
+  const idCard = useAppSelector(CardIdSettingSelector)
+  const question = useAppSelector(questionSettingSelector)
+  const statusLoad = useAppSelector(appStatusSelector)
+
+  const deleteCurrentCardHandler = () => {
+    dispatch(deleteCard({ id: idCard }))
+      .unwrap()
+      .then(() => {
+        closeModal()
+        dispatch(setSettingDeleteCardModal({ cardId: '', question: '' }))
+      })
+  }
+
+  return (
+    <Box sx={deleteCardContainerSx}>
+      <Typography component="p" sx={warningMessageSx}>
+        Do you really want to remove <strong>{question}</strong> <br />
+        Question will be deleted.
+      </Typography>
+      <Box sx={deleteCardBtnContainerSx}>
+        <GeneralButton
+          name="Cancel"
+          onClick={closeModal}
+          disabled={statusLoad === 'loading'}
+          sx={cancelBtnSx}
+        />
+        <GeneralButton
+          sx={deleteBtnSx}
+          name="Delete"
+          onClick={deleteCurrentCardHandler}
+          disabled={statusLoad === 'loading'}
+        />
+      </Box>
+    </Box>
+  )
+}
