289a28cb119a3ab238207190f736a1286b2d6f14
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-23 13:15:56 +0300

    feat(edit-card) : modal for edit card

---
+++ src/features/Cards/Modals-cards/EditCardModal/EditCardModal.tsx
@@ -0,0 +1,129 @@
+import React, { ChangeEvent, FC, useState } from 'react'
+
+import {
+  Box,
+  FormControl,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  TextField,
+  Typography,
+} from '@mui/material'
+
+import { setSettingEditCardModal, updateCard } from '../../cards-slice'
+
+import {
+  cancelBtn,
+  chooseQuestionTextSx,
+  editCardBtnContainerSx,
+  editCardContainerSx,
+  editSelectSx,
+  saveBtn,
+  textFieldSx,
+} from './editCardModal.muiSx'
+
+import { GeneralButton, useAppDispatch, useAppSelector } from 'common'
+
+export const EditCardModal: FC<{ closeModal: () => void }> = ({ closeModal }) => {
+  const dispatch = useAppDispatch()
+  const currentQuestion = useAppSelector(state => state.cards.cardsSettingModal.question)
+  const currentAnswer = useAppSelector(state => state.cards.cardsSettingModal.answer)
+  const idCard = useAppSelector(state => state.cards.cardsSettingModal.cardId)
+  const [questionFormat, setQuestionFormat] = useState('Text')
+  const [question, setQuestion] = useState(currentQuestion)
+  const [errorQuestion, setErrorQuestion] = useState('')
+  const [answer, setAnswer] = useState(currentAnswer)
+  const [errorAnswer, setErrorAnswer] = useState('')
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setQuestionFormat(event.target.value as string)
+  }
+
+  const editCardHandler = () => {
+    if (question === '' && answer === '') {
+      setErrorQuestion('can not be empty')
+      setErrorAnswer('can not be empty')
+    }
+    if (question === '') {
+      setErrorQuestion('can not be empty')
+    }
+    if (answer === '') {
+      setErrorAnswer('can not be empty')
+    }
+
+    if (question !== '' && answer !== '') {
+      const updateCurrentPack = {
+        _id: idCard,
+        question,
+        answer,
+      }
+
+      dispatch(updateCard(updateCurrentPack))
+        .unwrap()
+        .then(() => {
+          closeModal()
+          dispatch(setSettingEditCardModal({ cardId: '', answer: '', question: '' }))
+        })
+    }
+  }
+
+  const onChangeQuestion = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const question = e.currentTarget.value
+
+    setQuestion(question)
+    setErrorQuestion('')
+  }
+  const onChangeAnswer = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const answer = e.currentTarget.value
+
+    setAnswer(answer)
+    setErrorAnswer('')
+  }
+
+  return (
+    <Box sx={editCardContainerSx}>
+      <FormControl fullWidth>
+        <Typography component="p" sx={chooseQuestionTextSx}>
+          Choose a question format
+        </Typography>
+        <Select
+          size="small"
+          id="simple-select"
+          value={questionFormat}
+          onChange={handleChange}
+          sx={editSelectSx}
+        >
+          <MenuItem value={'Text'}>Text</MenuItem>
+        </Select>
+      </FormControl>
+
+      <TextField
+        fullWidth
+        type="text"
+        variant="standard"
+        value={question}
+        label="Question"
+        onChange={onChangeQuestion}
+        error={!!errorQuestion}
+        helperText={errorQuestion}
+        sx={textFieldSx}
+      />
+      <TextField
+        fullWidth
+        type="text"
+        variant="standard"
+        value={answer}
+        label="Answer"
+        onChange={onChangeAnswer}
+        error={!!errorAnswer}
+        helperText={errorAnswer}
+        sx={textFieldSx}
+      />
+
+      <Box sx={editCardBtnContainerSx}>
+        <GeneralButton name="Cancel" onClick={closeModal} sx={cancelBtn} />
+        <GeneralButton name="Save" onClick={editCardHandler} sx={saveBtn} />
+      </Box>
+    </Box>
+  )
+}
---
+++ src/features/Cards/Modals-cards/EditCardModal/editCardModal.muiSx.ts
@@ -0,0 +1,17 @@
+export const editCardContainerSx = { marginTop: '21px' }
+export const chooseQuestionTextSx = {
+  fontWeight: '14px',
+  fontSize: '14px',
+  lineHeight: '17px',
+  opacity: '0.5',
+}
+export const editSelectSx = { marginTop: '8px' }
+export const textFieldSx = { marginTop: '23px' }
+export const editCardBtnContainerSx = {
+  margin: '34px 0 47px',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+}
+export const cancelBtn = { padding: '8px 28px', background: '#FCFCFC', color: '#000000' }
+export const saveBtn = { padding: '8px 44px' }
