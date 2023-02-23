5b0b366b49a5113b58d9a0b34d72b15e056bc40f
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-23 14:01:18 +0300

    feat(delete-card-pack): window to delete the whole deck inside the deck

---
+++ src/features/Cards/Modals-cards/DeleteCardPackModal/DeleteCardPackModal.muiSx.ts
@@ -0,0 +1,10 @@
+export const deletePackCardContainerSx = { marginTop: '30px' }
+export const warningMessageSx = { fontWeight: '400', fontSize: '14px', lineHeight: '24px' }
+export const deleteCardPackBtnContainerSx = {
+  margin: '34px 0 47px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+}
+export const cancelBtnSx = { padding: '8px 28px', background: '#FCFCFC', color: '#000000' }
+export const deleteBtnSx = { padding: '8px 28px', background: '#FF3636', color: '#FFFFFF' }
---
+++ src/features/Cards/Modals-cards/DeleteCardPackModal/DeleteCardPackModal.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+import { Box, Typography } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+
+import { deleteCardPack } from '../../cards-slice'
+
+import {
+  cancelBtnSx,
+  deleteBtnSx,
+  deleteCardPackBtnContainerSx,
+  deletePackCardContainerSx,
+  warningMessageSx,
+} from './DeleteCardPackModal.muiSx'
+
+import {
+  appStatusSelector,
+  GeneralButton,
+  packIdSelector,
+  packNameCardSelector,
+  PATH,
+  useAppDispatch,
+  useAppSelector,
+} from 'common'
+
+export const DeleteCardPackModal: React.FC<{ closeModal: () => void }> = ({ closeModal }) => {
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
+  const packId = useAppSelector(packIdSelector)
+  const cardPackName = useAppSelector(packNameCardSelector)
+  const statusLoad = useAppSelector(appStatusSelector)
+
+  const deleteCurrentPackCard = () => {
+    dispatch(deleteCardPack(packId))
+      .unwrap()
+      .then(() => {
+        closeModal()
+        navigate(PATH.PACKS)
+      })
+  }
+
+  return (
+    <Box sx={deletePackCardContainerSx}>
+      <Typography component="p" sx={warningMessageSx}>
+        Do you really want to remove <strong>{cardPackName}</strong> <br />
+        All cards will be deleted.
+      </Typography>
+      <Box sx={deleteCardPackBtnContainerSx}>
+        <GeneralButton
+          name="Cancel"
+          onClick={closeModal}
+          disabled={statusLoad === 'loading'}
+          sx={cancelBtnSx}
+        />
+        <GeneralButton
+          sx={deleteBtnSx}
+          name="Delete"
+          onClick={deleteCurrentPackCard}
+          disabled={statusLoad === 'loading'}
+        />
+      </Box>
+    </Box>
+  )
+}
