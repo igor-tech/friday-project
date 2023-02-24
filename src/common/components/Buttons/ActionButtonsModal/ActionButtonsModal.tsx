e44b09c3ce94842b5b34b83d3ea89657c777a636
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-24 18:34:24 +0300

    feat(action-buttons-modal): universal action buttons for modal window

---
+++ src/common/components/Buttons/ActionButtonsModal/ActionButtonsModal.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+import { useAppSelector } from '../../../hooks'
+import { appStatusSelector } from '../../../selectors'
+import { GeneralButton } from '../GeneralButton/GeneralButton'
+
+import { cancelBtnSx, submitBtnSx } from './actionButtonsModal.muiSx'
+
+interface PropsType {
+  cancelName: string
+  submitName: string
+  actionSubmit: () => void
+  closeModal: () => void
+  cancelStyleSx: React.CSSProperties
+  submitStyleSx: React.CSSProperties
+  error: string
+}
+
+export const ActionButtonsModal: React.FC<Partial<PropsType>> = ({
+  cancelStyleSx,
+  submitStyleSx,
+  error,
+  cancelName = 'Cancel',
+  submitName = 'Save',
+  actionSubmit,
+  closeModal,
+}) => {
+  const statusLoad = useAppSelector(appStatusSelector)
+  const defCancelBtnSx = {
+    ...cancelBtnSx,
+    ...cancelStyleSx,
+  }
+
+  const defSubmitBtnSx = {
+    ...submitBtnSx,
+    ...submitStyleSx,
+  }
+
+  return (
+    <>
+      <GeneralButton
+        name={cancelName}
+        sx={defCancelBtnSx}
+        onClick={closeModal}
+        disabled={statusLoad === 'loading'}
+      />
+      <GeneralButton
+        name={submitName}
+        sx={defSubmitBtnSx}
+        onClick={actionSubmit}
+        disabled={statusLoad === 'loading' || !!error}
+      />
+    </>
+  )
+}
