5ab2f0d612bc24f06a71f6473bb09c45999ae492
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-24 19:56:50 +0300

    refactor(modals-content): replaced the markup through a check with a constant that returns the desired component

--- src/common/components/Modal/ModalContent/ModalContent.tsx
+++ src/common/components/Modal/ModalContent/ModalContent.tsx
@@ -1,33 +1,17 @@
 import React from 'react'
 
-import { MODAL_TYPE } from '../../../constants'
+import { MODALS } from '../../../constants'
 import { useAppSelector } from '../../../hooks'
 import { modalTypeSelector } from '../../../selectors'
-
-import {
-  AddNewPackModal,
-  DeletePackModal,
-  EditPackModal,
-  AddNewCardModal,
-  EditCardModal,
-  DeleteCardModal,
-  DeleteCardPackModal,
-  EditCardPackModal,
-} from 'features'
 
 export const ModalContent = () => {
   const typeModal = useAppSelector(modalTypeSelector)
 
+  const Modal = MODALS[typeModal]
+
   return (
     <>
-      {typeModal === MODAL_TYPE.addNewPack && <AddNewPackModal />}
-      {typeModal === MODAL_TYPE.editCurrentPack && <EditPackModal />}
-      {typeModal === MODAL_TYPE.deleteCurrentPack && <DeletePackModal />}
-      {typeModal === MODAL_TYPE.addNewCard && <AddNewCardModal />}
-      {typeModal === MODAL_TYPE.editCurrentCard && <EditCardModal />}
-      {typeModal === MODAL_TYPE.editCurrentPackCard && <EditCardPackModal />}
-      {typeModal === MODAL_TYPE.deleteCurrentCard && <DeleteCardModal />}
-      {typeModal === MODAL_TYPE.deleteCurrentPackCard && <DeleteCardPackModal />}
+      <Modal />
     </>
   )
 }
