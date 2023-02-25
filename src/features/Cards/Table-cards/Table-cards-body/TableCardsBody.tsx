9c0095ea4ebf778de1d3e5f0dae757aa9cbff0ba
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-25 16:49:15 +0300

    refactor(table-card): add tooltip for icons actions

--- src/features/Cards/Table-cards/Table-cards-body/TableCardsBody.tsx
+++ src/features/Cards/Table-cards/Table-cards-body/TableCardsBody.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { DriveFileRenameOutlineOutlined, DeleteOutlined } from '@mui/icons-material'
-import { TableRow, TableCell, TableBody, Box, IconButton, Rating } from '@mui/material'
+import { TableRow, TableCell, TableBody, Box, IconButton, Rating, Tooltip } from '@mui/material'
 
 import { useTableCardsBody } from '../hooks/useTableCardsBody'
 
@@ -29,29 +29,38 @@ export const TableCardsBody = () => {
           <Rating name="read-only" value={cards.grade} readOnly />
           {isMyPack && (
             <Box sx={btnIcon}>
-              <IconButton
-                onClick={() =>
-                  updateCurrentCardHandler(
-                    cards._id,
-                    cards.question,
-                    cards.answer,
-                    MODAL_TYPE.editCurrentCard
-                  )
-                }
-                disabled={statusLoad === 'loading'}
-                sx={btnIconBlack}
-              >
-                <DriveFileRenameOutlineOutlined />
-              </IconButton>
-              <IconButton
-                onClick={() =>
-                  deleteCurrentCardHandler(cards._id, cards.question, MODAL_TYPE.deleteCurrentCard)
-                }
-                disabled={statusLoad === 'loading'}
-                sx={btnIconBlack}
-              >
-                <DeleteOutlined />
-              </IconButton>
+              <Tooltip title="edit card" placement="top" arrow>
+                <IconButton
+                  onClick={() =>
+                    updateCurrentCardHandler(
+                      cards._id,
+                      cards.question,
+                      cards.answer,
+                      MODAL_TYPE.editCurrentCard
+                    )
+                  }
+                  disabled={statusLoad === 'loading'}
+                  sx={btnIconBlack}
+                >
+                  <DriveFileRenameOutlineOutlined />
+                </IconButton>
+              </Tooltip>
+
+              <Tooltip title="edit card" placement="top" arrow>
+                <IconButton
+                  onClick={() =>
+                    deleteCurrentCardHandler(
+                      cards._id,
+                      cards.question,
+                      MODAL_TYPE.deleteCurrentCard
+                    )
+                  }
+                  disabled={statusLoad === 'loading'}
+                  sx={btnIconBlack}
+                >
+                  <DeleteOutlined />
+                </IconButton>
+              </Tooltip>
             </Box>
           )}
         </TableCell>
