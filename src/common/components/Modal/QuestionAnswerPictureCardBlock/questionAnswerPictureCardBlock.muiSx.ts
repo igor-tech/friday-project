317882a00d369c11fcba69707ae951c2ea1df1dd
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-02-28 22:58:12 +0300

    feat(question-answer-picture-block): component to draw when image is selected

---
+++ src/common/components/Modal/QuestionAnswerPictureCardBlock/questionAnswerPictureCardBlock.muiSx.ts
@@ -0,0 +1,9 @@
+export const questionAnswerPictureContainerSx = {
+  marginTop: '21px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+}
+export const titleSx = { fontWeight: '500', fontSize: '16px', lineHeight: '20px' }
+export const questionAnswerImgSx = { width: '100%', height: '120px' }
+export const errorSx = { color: 'red' }
