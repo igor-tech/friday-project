6d576de43812ffb4d63dc0f31c1e8639a7e57268
Igor Shargin <igor20513@gmail.com>
2023-03-01 01:08:05 +0500

    feat(Table packs): take out column Name from TablePacks

---
+++ src/features/Packs/Table-packs/Table-packs-body/TableCellName.tsx
@@ -0,0 +1,41 @@
+import React, { FC, useState } from 'react'
+
+import { Box, Link, TableCell } from '@mui/material'
+import { NavLink } from 'react-router-dom'
+
+import defaultCover from '../../../../assets/img/defaultCover.jpg'
+import { PATH } from '../../../../common'
+import { CardsPack } from '../../table-api'
+import { firstColumnSx, userLinkSx } from '../TablePacks.muiSx'
+
+type PropsType = {
+  labelId: string
+  cardsPack: CardsPack
+}
+export const TableCellName: FC<PropsType> = ({ labelId, cardsPack }) => {
+  const [isCoverBroken, setIsCoverBroken] = useState(false)
+  const errorHandler = () => {
+    setIsCoverBroken(true)
+  }
+  const deckCover = cardsPack.deckCover ? cardsPack.deckCover : defaultCover
+
+  return (
+    <TableCell id={labelId}>
+      <Box sx={firstColumnSx}>
+        <img
+          style={{ height: '36px', borderRadius: '2px', mixBlendMode: 'normal' }}
+          src={isCoverBroken ? defaultCover : deckCover}
+          onError={errorHandler}
+          alt={'deck cover'}
+        />
+        <Link
+          component={NavLink}
+          to={`${PATH.PACKS}${PATH.CARDS}?cardsPackId=${cardsPack._id}`}
+          sx={userLinkSx}
+        >
+          {cardsPack.name}
+        </Link>
+      </Box>
+    </TableCell>
+  )
+}
